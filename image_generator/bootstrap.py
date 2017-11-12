80c64ecc171f91fc9598d2ce106dfe746b928d01
Florian Stellbrink <florian@stellbr.ink>
2017-11-12 04:07:00 +0100

    Fix bootstrapper when started from blender

--- image_generator/bootstrap.py
+++ image_generator/bootstrap.py
@@ -4,10 +4,13 @@ import sys
 # Bootstrap main script in blender
 
 # Setup working directory
-cwd = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(cwd)
-os.chdir(cwd)
+if bpy.context.space_data is not None:
+    working_directory = os.path.dirname(bpy.context.space_data.text.filepath)
+else:
+    working_directory = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(working_directory)
+os.chdir(working_directory)
 
 # Compile and execute script file
-file = os.path.join(cwd, 'main.py')
+file = os.path.join(working_directory, 'main.py')
 exec(compile(open(file).read(), 'main.py', 'exec'))
